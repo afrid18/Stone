5486ac4b8a819dcfa678b0a1d170f341caf9ea44
Jagadeesh Gongidi <jagadeeshgoud001@gmail.com>
2023-09-07 20:31:36 +0530

    implemented basic add functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import { useState, useRef } from "react"
 function App() {
   const inputRef = useRef(null)
-  const [todoItems, setTodoItems] = useState(localStorage.getItem(todos) || [])
+  const [todoItems, setTodoItems] = useState(JSON.parse(localStorage.getItem('todos')) || [])
   function todoHandler() {
-
-
+    const todo = inputRef.current.value;
+    setTodoItems((prev) => {
+      return [...prev, { 'text': todo }]
+    })
+    localStorage.setItem('todos', JSON.stringify(todoItems))
+    inputRef.current.value = "";
   }
   return (
     <div>
-      <input type="text" onClick={todoHandler} ref={inputRef} />
-
+      <label for="todoItem">Toodo</label>
+      <input type="text" id="todoItem" ref={inputRef} />
+      <button onClick={todoHandler}>Add</button>
+      {
+        todoItems.map((todo, index) => {
+          return <p className="todo__item">{todo.text}</p>
+        })
+      }
     </div>
   )
 }
