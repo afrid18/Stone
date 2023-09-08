42f2ca25bc4933b5a6009c750e23b37b09ecadd9
Jagadeesh Gongidi <jagadeeshgoud001@gmail.com>
2023-09-08 23:15:16 +0530

    added todo adding functionality with localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,40 @@
+import { useState, useRef, useEffect } from "react";
+import "./App.css"
 function App() {
+  const inputRef = useRef(null)
+  const [todoItems, setTodoItems] = useState(JSON.parse(localStorage.getItem('todos')) || [])
+  function todoHandler() {
+    const todo = inputRef.current.value;
+    if (!todo) return
+    setTodoItems((prev) => {
+      return [...prev, { 'text': todo, timeStamp: new Date().toLocaleTimeString() }]
+    })
 
+  }
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todoItems))
+    inputRef.current.value = "";
+  }, [todoItems])
   return (
-    <div>
-      <h1> Hello World! </h1>
-    </div>
+    <>
+      <h1 className="heading">Make things Happen</h1>
+      <div className="todo">
+        <form className="row a-center j-ccenter">
+          <label htmlFor="todoItem" >Toodo</label>
+          <input required type="text" className="todo__input" id="todoItem" ref={inputRef} />
+          <button className="todo__add-btn" onClick={todoHandler}>Add</button>
+        </form>
+        <ul role="list" className="todo__list">
+          {
+            todoItems.map((todo) => {
+              return <li key={todo.timeStamp} className="todo__item"><span className="todo__text">{todo.text}</span><span title="added time" className="todo__date">{todo.timeStamp}</span></li>
+            })
+          }
+        </ul>
+
+      </div>
+    </>
   )
 }
 
